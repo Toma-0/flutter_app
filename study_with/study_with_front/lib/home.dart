03bef56a49f8f6fc788e106061f6851a7c4d62f0
toma-0 <toma0122yukako@icloud.com>
2023-01-27 21:06:06 +0900

    時間ごとに文字と画像が変更され文字と画像が変更される

--- study_with/study_with_front/lib/home.dart
+++ study_with/study_with_front/lib/home.dart
@@ -31,66 +31,105 @@ class home extends StatefulWidget {
 
 class _home extends State<home> {
   Image tmp = Image.asset("img/sinya_1.png");
+  String now_clock = "";
+
+  var now = DateTime.now();
+
+  int tmp_hour = 12;
+
+  int hour = 0;
+  int minute = 0;
+  int day = 0;
+  int month = 0;
+  int second = 0;
+
+  now_time() {
+    Timer.periodic(
+        const Duration(seconds: 1) //10minuteに変更必須
+        , (timer) {
+      setState(() {
+        now = DateTime.now();
+        month = now.month;
+        day = now.day;
+        hour = now.hour;
+        minute = now.minute;
+        second = now.second;
+        now_clock = month.toString().padLeft(2, "0") +
+            "月" +
+            day.toString().padLeft(2, "0") +
+            "日　" +
+            hour.toString().padLeft(2, "0") +
+            ":" +
+            minute.toString().padLeft(2, "0") +
+            ":" +
+            second.toString().padLeft(2, "0");
+      });
+    });
+  }
+
+  void timer() {
+
+    change_img() {//時間ごとにどの画像を使用するかの決定
+      var random = math.Random();
+      if (0 <= hour && hour <= 5) {
+        int index = random.nextInt(3);
+        setState(() {
+          tmp = Image.asset('img/sinya_$index.png');
+        });
+      } else if (6 <= hour && hour <= 11) {
+        int index = random.nextInt(7);
+        setState(() {
+          tmp = Image.asset('img/asa_$index.png');
+        });
+      } else if (12 <= hour && hour <= 14) {
+        int index = random.nextInt(7);
+        setState(() {
+          tmp = Image.asset('img/hiru_$index.png');
+        });
+      } else if (15 <= hour && hour <= 18) {
+        int index = random.nextInt(8);
+        setState(() {
+          tmp = Image.asset('img/yuu_$index.png');
+        });
+      } else if (19 <= hour && hour <= 23) {
+        int index = random.nextInt(7);
+        setState(() {
+          tmp = Image.asset('img/yoru_$index.png');
+        });
+      }
+    }
+
+    if (tmp_hour != hour) {
+      change_img();//画像の変更
+      tmp_hour = hour;
+    }
+  }
+
+  img() {
+    now_time();
+    timer();
+    return tmp;
+  }
+
+  clock_now() {
+    now_time();
+    return now_clock;
+  }
 
   Widget build(BuildContext context) {
-    time() {
-      var now = DateTime.now();
-      int hour = now.hour;
-      return hour;
-    }
-
-    void timer() {
-
-      change_img() {
-        int hour = time();
-        var random = math.Random();
-        if (0 <= hour && hour <= 5) {
-          int index = random.nextInt(3);
-          setState(() {
-            tmp = Image.asset('img/sinya_$index.png');
-          });
-        } else if (6 <= hour && hour <= 11) {
-          int index = random.nextInt(7);
-          setState(() {
-            tmp = Image.asset('img/asa_$index.png');
-          });
-        } else if (12 <= hour && hour <= 14) {
-          int index = random.nextInt(7);
-          setState(() {
-            tmp = Image.asset('img/hiru_$index.png');
-          });
-        } else if (15 <= hour && hour <= 18) {
-          int index = random.nextInt(8);
-          setState(() {
-            tmp = Image.asset('img/yuu_$index.png');
-          });
-        } else if (19 <= hour && hour <= 23) {
-          int index = random.nextInt(7);
-          setState(() {
-            tmp = Image.asset('img/yoru_$index.png');
-          });
-        }
-      }
-
-      change_img();
-
-      Timer.periodic(
-          const Duration(hours: 1) //10minuteに変更必須
-          , (timer) {
-        change_img();
-      });
-    }
-
-    img() {
-      timer();
-      return tmp;
-    }
-
     return Scaffold(
-      body: Center(
-          child: Container(
-        child: img(),
-      )),
-    );
+        body: Row(children: [
+      Column(
+        children: [
+          Text(clock_now()),
+        ],
+      ),
+      Container(
+        child: Center(
+            child: Container(
+          child: img(),
+        )),
+      )
+    ]));
   }
 }
