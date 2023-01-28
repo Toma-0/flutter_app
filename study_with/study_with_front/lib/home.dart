3ad673c2107260193c5a8f9b0485254539a0bedd
toma-0 <toma0122yukako@icloud.com>
2023-01-29 08:50:21 +0900

    sttの導入(iosだと英語になる問題あり)

--- study_with/study_with_front/lib/home.dart
+++ study_with/study_with_front/lib/home.dart
@@ -7,9 +7,8 @@ import 'firebase_options.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 
-import 'home.dart';
 import "sin_in.dart";
-import "timer.dart";
+import "test.dart";
 
 import 'config/size_config.dart';
 import 'config/color_config.dart';
