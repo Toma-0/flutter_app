a402662ed4506318c15ff6821cea689e820d84e5
toma-0 <toma0122yukako@icloud.com>
2023-02-05 05:49:02 +0900

    firebaseの初期設定

--- oshi_chokin/oshichokin_front/test/widget_test.dart
+++ oshi_chokin/oshichokin_front/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:oshichokin_front/main.dart';
+import 'package:oshichokin_front/Sign_in.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
