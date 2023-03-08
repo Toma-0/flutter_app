81ee02b8b927c796f015790ccdd3af5700ec8c73
toma-0 <toma0122yukako@icloud.com>
2023-03-09 07:39:43 +0900

    パーツ分け完了

--- oshi_chokin/oshichokin_front/lib/parts/form.dart
+++ oshi_chokin/oshichokin_front/lib/parts/form.dart
@@ -4,12 +4,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import "package:cloud_firestore/cloud_firestore.dart";
 
 class form {
+  //各種コントローラーの生成
   final titleController = TextEditingController();
   final moneyController = TextEditingController();
   final contentsController = TextEditingController();
   final _date = TextEditingController();
   DateTime selectedDate = DateTime.now();
 
+  //日付選択用Widgetが呼ばれたときの処理
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
         context: context,
@@ -19,13 +21,26 @@ class form {
     if (picked != null && picked != selectedDate) selectedDate = picked;
   }
 
-  void nyukin(ref, oshi) async {
+  //入金処理の実行
+  void nyukin(ref, oshi, syukkin) async {
+    //データベースで使用する関数の定義
+    String collection = "nyukin";
+
+    //入力フォームからの入力を変数に格納
     final title = titleController.text;
-    final money = int.parse(moneyController.text);
+    int money = int.parse(moneyController.text);
     final contents = contentsController.text;
     final today = DateTime.now();
     final dateOnly = DateTime(today.year, today.month, today.day);
 
+    if (syukkin) {
+      //出金の場合は金額にマイナスをつける
+      //コレクション名も変更する
+      money = money * -1;
+      collection = "syukkin";
+    }
+
+    //データベースに挿入するデータの定義
     Map<String, dynamic> data = {
       "created_at": dateOnly,
       "title": title,
@@ -38,14 +53,16 @@ class form {
         var db = FirebaseFirestore.instance;
         var user_id = user.uid;
 
+        //Firestoreにデータを追加
         final docRef = db
             .collection("users")
             .doc(user_id)
             .collection("oshi")
             .doc(oshi)
-            .collection("nyukin");
+            .collection(collection);
         await docRef.add(data);
 
+        //Firestore上のoshisumに金額を加算
         final oshiRef =
             db.collection("users").doc(user_id).collection("oshi").doc(oshi);
 
@@ -54,87 +71,37 @@ class form {
             .then((_) => print('Update success!'))
             .catchError((error) => print('Failed to update: $error'));
 
+        //状態管理されているoshiList、oshiIndex、oshiSumMoney、sumMoney の更新
         final userRef = db.collection("users").doc(user_id);
         late List oshiList = ref.read(oshiListProvider);
         late List indexList = ref.read(oshiIndexProvider);
+
+        //oshiList中におし名(oshi)が何番目にあるか(index) を検索
         int index = indexList[oshiList.indexOf(oshi)];
         List sumOshiList = ref.read(oshiSumMoneyProvider);
 
+        //該当するoshiの金額（sumOshiList[index]） にmoney分加算
         sumOshiList[index] = sumOshiList[index] + money;
         ref.read(oshiSumMoneyProvider.notifier).update((state) => sumOshiList);
 
+        //全てのOshiの金額の合計(sumMoney)にmoney分加算
         int sum = ref.read(sumMoneyProvider);
         sum = sum + money;
         ref.read(sumMoneyProvider.notifier).update((state) => sum);
 
+        //Firestore上のsum_moneyに金額を加算
         userRef
             .update({'sum_money': FieldValue.increment(money)})
             .then((_) => print('Update success!'))
             .catchError((error) => print('Failed to update: $error'));
       } else {
+        //ログインしていない場合
         print("user:$user");
       }
     });
   }
 
-  void syukkin(ref, oshi, nyuusyutu) async {
-    final title = titleController.text;
-    final money = int.parse(moneyController.text);
-    final contents = contentsController.text;
-    final today = DateTime.now();
-    final dateOnly = DateTime(today.year, today.month, today.day);
-
-    Map<String, dynamic> data = {
-      "created_at": dateOnly,
-      "title": title,
-      "money": -money,
-      "contents": contents
-    };
-
-    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-      if (user != null) {
-        var db = FirebaseFirestore.instance;
-        var user_id = user.uid;
-
-        final docRef = db
-            .collection("users")
-            .doc(user_id)
-            .collection("oshi")
-            .doc(oshi)
-            .collection("syukkin");
-        await docRef.add(data);
-
-        final oshiRef =
-            db.collection("users").doc(user_id).collection("oshi").doc(oshi);
-
-        oshiRef
-            .update({'sum_money': FieldValue.increment(-money)})
-            .then((_) => print('Update success!'))
-            .catchError((error) => print('Failed to update: $error'));
-
-        final userRef = db.collection("users").doc(user_id);
-        late List oshiList = ref.read(oshiListProvider);
-        late List indexList = ref.read(oshiIndexProvider);
-        int index = indexList[oshiList.indexOf(oshi)];
-        List sumOshiList = ref.read(oshiSumMoneyProvider);
-
-        sumOshiList[index] = sumOshiList[index] - money;
-        ref.read(oshiSumMoneyProvider.notifier).update((state) => sumOshiList);
-
-        int sum = ref.read(sumMoneyProvider);
-        sum = sum - money;
-        ref.read(sumMoneyProvider.notifier).update((state) => sum);
-
-        userRef
-            .update({'sum_money': FieldValue.increment(-money)})
-            .then((_) => print('Update success!'))
-            .catchError((error) => print('Failed to update: $error'));
-      } else {
-        print("user:$user");
-      }
-    });
-  }
-
+  //フォームを表示する
   formList(
     List list,
   ) {
@@ -147,6 +114,8 @@ class form {
     );
   }
 
+//textフォームを指定する。
+//行数、高さ、いろ、コントローラー、アイコン、テキストを指定する
   _form(i, y, oshicolor, controller, icon, text) {
     return SizedBox(
       height: y,
