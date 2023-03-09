7d783593951e9c8768728cef8630266ff23788f7
toma-0 <toma0122yukako@icloud.com>
2023-03-09 19:56:08 +0900

    importの整理

---
+++ oshi_chokin/oshichokin_front/lib/state/import.dart
@@ -0,0 +1,38 @@
+// materialパッケージのインポート
+export 'package:flutter/material.dart';
+export 'package:oshichokin_front/config/size_config.dart';
+
+//フォント関連のインポート
+export 'package:google_fonts/google_fonts.dart';
+
+//カレンダー作成
+export 'package:table_calendar/table_calendar.dart';
+
+// Firebase関連のライブラリーのインポート
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export "package:cloud_firestore/cloud_firestore.dart";
+export 'package:oshichokin_front/firebase_options.dart';
+
+
+// Riverpod関連のライブラリーのインポート
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// ユーザ情報の取得用クラスのインポート
+export '../info/user_info.dart';
+export '../info/oshi_info.dart';
+export '../info/oshi_images.dart';
+
+// 状態(State)管理
+export 'state.dart';
+
+//各パーツ情報
+export '../parts/calender.dart';
+export '../parts/waveAnime.dart';
+export '../parts/donutsChart.dart';
+export "../parts/slide.dart";
+export "../parts/slide_parts.dart";
+export "../parts/form.dart";
+
+
+
